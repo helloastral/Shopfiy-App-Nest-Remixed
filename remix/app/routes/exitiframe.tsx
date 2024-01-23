ee681e13a456f0588d8fece4db559d300e22c34e
tapan <tapan@storetools.io>
2024-01-23 18:15:07 +0530

    Adds loading & fixes redirect

---
+++ remix/app/routes/exitiframe.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import { Banner, Layout, Page, Spinner } from "@shopify/polaris";
+import { useSearchParams } from "@remix-run/react";
+
+export default function ExitIframe() {
+  const [search] = useSearchParams();
+  const [showWarning, setShowWarning] = useState(false);
+
+  useEffect(() => {
+    if (search) {
+      const redirectUri = search.get("redirectUri") as string;
+      const url = new URL(decodeURIComponent(redirectUri));
+
+      if (
+        [location.hostname, "admin.shopify.com"].includes(url.hostname) ||
+        url.hostname.endsWith(".myshopify.com")
+      ) {
+        const url = decodeURIComponent(redirectUri);
+        window.open(url, "_top");
+      } else {
+        setShowWarning(true);
+      }
+    }
+  }, [search, setShowWarning]);
+
+  return showWarning ? (
+    <Page narrowWidth>
+      <Layout>
+        <Layout.Section>
+          <div style={{ marginTop: "100px" }}>
+            <Banner title="Redirecting outside of Shopify" tone="warning">
+              Apps can only use /exitiframe to reach Shopify or the app itself.
+            </Banner>
+          </div>
+        </Layout.Section>
+      </Layout>
+    </Page>
+  ) : (
+    <Spinner />
+  );
+}
