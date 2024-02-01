5108a38f19ccf268bad832054c5582e87eddb799
Geon George <geongeorge98@gmail.com>
2024-02-01 12:55:49 +0530

    Fixes server redirect

--- remix/app/libs/api.server.ts
+++ remix/app/libs/api.server.ts
@@ -1,45 +1,45 @@
-import axios, { AxiosResponseHeaders } from "axios";
-import { LoaderFunctionArgs, redirect } from "@remix-run/node";
+import axios, { AxiosResponseHeaders } from 'axios'
+import { LoaderFunctionArgs, redirect } from '@remix-run/node'
 
-const host = process.env.HOST;
+const host = process.env.HOST
 
-export async function getApi(request: LoaderFunctionArgs["request"]) {
+export async function getApi(request: LoaderFunctionArgs['request']) {
   const instance = axios.create({
-    baseURL: "/api",
-  });
+    baseURL: '/api',
+  })
 
   instance.interceptors.request.use(async (config) => {
-    const { searchParams } = new URL(request.url);
-    const token = searchParams.get("id_token");
-    config.headers["Authorization"] = `Bearer ${token}`;
-    config.headers["X-Requested-With"] = "XMLHttpRequest";
+    const { searchParams } = new URL(request.url)
+    const token = searchParams.get('id_token')
+    config.headers['Authorization'] = `Bearer ${token}`
+    config.headers['X-Requested-With'] = 'XMLHttpRequest'
 
-    return config;
-  });
+    return config
+  })
 
   instance.interceptors.response.use(
     (response) => {
-      return response;
+      return response
     },
     (error) => {
       if (error.response.headers) {
-        checkHeadersForReauthorization(error.response.headers);
+        return checkHeadersForReauthorization(error.response.headers)
       }
-      return Promise.reject(error);
-    }
-  );
+      return Promise.reject(error)
+    },
+  )
 
-  return instance;
+  return instance
 }
 
 function checkHeadersForReauthorization(headers: AxiosResponseHeaders) {
   const authUrlHeader =
-    (headers.get("X-Shopify-API-Request-Failure-Reauthorize-Url") as string) ||
-    `/api/auth`;
+    (headers.get('X-Shopify-API-Request-Failure-Reauthorize-Url') as string) ||
+    `/api/auth`
 
   return redirect(
-    authUrlHeader.startsWith("/")
+    authUrlHeader.startsWith('/')
       ? `https://${host}${authUrlHeader}`
-      : authUrlHeader
-  );
+      : authUrlHeader,
+  )
 }
