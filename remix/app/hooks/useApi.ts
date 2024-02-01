b6ce552595c4e138d1cd9b2417e0b6375ec32cbc
tapan <tapan@storetools.io>
2024-02-01 18:21:21 +0530

    fixes auth redirect

--- remix/app/hooks/useApi.ts
+++ remix/app/hooks/useApi.ts
@@ -1,55 +1,53 @@
-import axios, { AxiosResponseHeaders } from "axios";
-import { useMemo } from "react";
+import axios, { AxiosResponseHeaders } from 'axios'
+import { useMemo } from 'react'
 
 export function useApi() {
   const api = useMemo(() => {
     const instance = axios.create({
-      baseURL: "/api",
-    });
+      baseURL: '/api',
+    })
 
     // To check if the token is set or wait the request until it's set
     instance.interceptors.request.use(async (config) => {
-      const token = await shopify.idToken();
+      const token = await shopify.idToken()
 
-      config.headers["Authorization"] = `Bearer ${token}`;
-      config.headers["X-Requested-With"] = "XMLHttpRequest";
+      config.headers['Authorization'] = `Bearer ${token}`
+      config.headers['X-Requested-With'] = 'XMLHttpRequest'
 
-      return config;
-    });
+      return config
+    })
 
     instance.interceptors.response.use(
       (response) => {
-        checkHeadersForReauthorization(
-          response.headers as AxiosResponseHeaders
-        );
-        return response;
+        checkHeadersForReauthorization(response.headers as AxiosResponseHeaders)
+        return response
       },
       (error) => {
         if (error.response.headers) {
-          checkHeadersForReauthorization(error.response.headers);
+          checkHeadersForReauthorization(error.response.headers)
         }
-        return Promise.reject(error);
-      }
-    );
+        return Promise.reject(error)
+      },
+    )
 
-    return instance;
-  }, []);
+    return instance
+  }, [])
 
-  return { api };
+  return { api }
 }
 
 function checkHeadersForReauthorization(headers: AxiosResponseHeaders) {
-  if (headers.get("X-Shopify-API-Request-Failure-Reauthorize") === "1") {
+  if (headers.get('X-Shopify-API-Request-Failure-Reauthorize') === '1') {
     const authUrlHeader =
       (headers.get(
-        "X-Shopify-API-Request-Failure-Reauthorize-Url"
-      ) as string) || `/api/auth`;
+        'X-Shopify-API-Request-Failure-Reauthorize-Url',
+      ) as string) || `/api/auth`
 
     window.open(
-      authUrlHeader.startsWith("/")
+      authUrlHeader.startsWith('/')
         ? `https://${window.location.host}${authUrlHeader}`
         : authUrlHeader,
-      "_self"
-    );
+      '_top',
+    )
   }
 }
